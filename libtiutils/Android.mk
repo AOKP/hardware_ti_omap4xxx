e156c9ab0aacc96563ee06acdc43edfa6594f8ac
Jeff Sharkey <jsharkey@android.com>
2011-08-31 17:49:32 -0700

    Remove unused includes.
    Change-Id: Ie19b949f3c6a2b2080400bf76c0ea6501a0cd6b4

--- libtiutils/Android.mk
+++ libtiutils/Android.mk
@@ -19,7 +19,6 @@ LOCAL_SHARED_LIBRARIES:= \
     libcutils
 
 LOCAL_C_INCLUDES += \
-        kernel/android-2.6.29/include \
 	frameworks/base/include/utils \
 	bionic/libc/include \
 	hardware/ti/omap4xxx/domx/omx_core/inc \
