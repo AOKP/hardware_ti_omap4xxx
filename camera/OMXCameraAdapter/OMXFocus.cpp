3c272a993dfc4ca153b09e5ec1e651f75eab90c4
Milen Mitkov <mmitkov@mm-sol.com>
2011-10-28 16:26:35 +0300

    CameraHAL: Return status after CAF finishes
    If the user initiates autofocus while CAF
    is scanning (e.g. when capturing),
    wait until CAF finishes and just then
    notify the focus subscribers.
    
    bug:5514415
    
    Change-Id: I971783c74f9977755cc93cdba964e4fcd3a2ab0a
    Signed-off-by: Milen Mitkov <mmitkov@mm-sol.com>

--- camera/OMXCameraAdapter/OMXFocus.cpp
+++ camera/OMXCameraAdapter/OMXFocus.cpp
@@ -149,9 +149,10 @@ status_t OMXCameraAdapter::doAutoFocus()
         }
     }
 
-    if ( ( focusControl.eFocusControl != OMX_IMAGE_FocusControlAuto ) &&
-         ( focusControl.eFocusControl != ( OMX_IMAGE_FOCUSCONTROLTYPE )
-                 OMX_IMAGE_FocusControlAutoInfinity ) ) {
+    if ( (focusControl.eFocusControl == OMX_IMAGE_FocusControlAuto
+            && focusStatus.eFocusStatus == OMX_FocusStatusRequest) ||
+            (mParameters3A.Focus !=  (OMX_IMAGE_FOCUSCONTROLTYPE)OMX_IMAGE_FocusControlAuto) )
+        {
 
         ret = RegisterForEvent(mCameraAdapterParameters.mHandleComp,
                                     (OMX_EVENTTYPE) OMX_EventIndexSettingChanged,
@@ -159,13 +160,12 @@ status_t OMXCameraAdapter::doAutoFocus()
                                     OMX_IndexConfigCommonFocusStatus,
                                     mDoAFSem);
 
-        if ( NO_ERROR == ret ) {
-            ret = setFocusCallback(true);
-        }
-
-        eError =  OMX_SetConfig(mCameraAdapterParameters.mHandleComp,
-                                OMX_IndexConfigFocusControl,
-                                &focusControl);
+        if ( focusControl.eFocusControl != OMX_IMAGE_FocusControlAuto )
+            {
+            eError =  OMX_SetConfig(mCameraAdapterParameters.mHandleComp,
+                                    OMX_IndexConfigFocusControl,
+                                    &focusControl);
+            }
 
         if ( OMX_ErrorNone != eError ) {
             CAMHAL_LOGEB("Error while starting focus 0x%x", eError);
@@ -181,9 +181,6 @@ status_t OMXCameraAdapter::doAutoFocus()
                 return EINVAL;
             }
 
-            //Disable auto focus callback from Ducati
-            setFocusCallback(false);
-            CAMHAL_LOGEA("Autofocus callback timeout expired");
             RemoveEvent(mCameraAdapterParameters.mHandleComp,
                                         (OMX_EVENTTYPE) OMX_EventIndexSettingChanged,
                                         OMX_ALL,
@@ -191,9 +188,7 @@ status_t OMXCameraAdapter::doAutoFocus()
                                         NULL );
             returnFocusStatus(true);
         } else {
-            CAMHAL_LOGDA("Autofocus callback received");
-            //Disable auto focus callback from Ducati
-            setFocusCallback(false);
+
             ret = returnFocusStatus(false);
         }
     } else { // Focus mode in continuous
@@ -233,12 +228,6 @@ status_t OMXCameraAdapter::stopAutoFocus()
         // No need to stop focus if we are in infinity mode. Nothing to stop.
         return NO_ERROR;
     }
-
-    if ( NO_ERROR == ret )
-       {
-       //Disable the callback first
-       ret = setFocusCallback(false);
-       }
 
     if ( NO_ERROR == ret )
         {
