0aead355133c2a087cb369c5eb62a07cbd4a13be
Lajos Molnar <lajos@ti.com>
2011-12-09 10:09:17 -0600

    hwc: added static image support
    Display static image if connected to portrait dock and there are no
    dockable layers.
    
    Change-Id: I572cf9ef5bf66ac194c3a83920a8de3be1e6d4e4
    Signed-off-by: Lajos Molnar <lajos@ti.com>

--- hwc/Android.mk
+++ hwc/Android.mk
@@ -7,13 +7,15 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/../vendor/lib/hw
-LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy
+LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy libz
 LOCAL_SRC_FILES := hwc.c
+LOCAL_STATIC_LIBRARIES := libpng
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := hwcomposer.omap4
 LOCAL_CFLAGS := -DLOG_TAG=\"ti_hwc\"
+LOCAL_C_INCLUDES += external/libpng external/zlib
 # LOG_NDEBUG=0 means verbose logging enabled
 # LOCAL_CFLAGS += -DLOG_NDEBUG=0
 include $(BUILD_SHARED_LIBRARY)
