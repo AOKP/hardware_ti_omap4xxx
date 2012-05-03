9835bb9484acbc86986070adc09eb4af56ac0fb7
Jean-Baptiste Queru <jbq@google.com>
2012-05-02 18:02:39 -0700

    liboemcrypto only exists in vendor/ builds
    Change-Id: Ied82b1f8dc177feb710e8d9b0abd42fbfac4d9f9

--- libdrmdecrypt/Android.mk
+++ libdrmdecrypt/Android.mk
@@ -9,8 +9,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/hardware \
         $(TOP)/vendor/widevine/proprietary/cryptoPlugin \
 
-LOCAL_STATIC_LIBRARIES := \
-        liboemcrypto                    \
+LOCAL_STATIC_LIBRARIES += \
         libtee_client_api_driver        \
 
 LOCAL_SHARED_LIBRARIES := \
